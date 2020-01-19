9c59e3959b79dd77aa1c430626945a32d0825522
kasperskov01 <kasperskov01@gmail.com>
2020-01-19 21:19:42 +0100

    ThankYouPage

--- src/components/EmailPopup.js
+++ src/components/EmailPopup.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Form, Button, Col, Row } from 'react-bootstrap';
 import '../assets/sass/_email_sub.scss';
+import { StaticRouter as Router, Route } from 'react-router-dom';
 
 
 export default class EmailPopup extends Component {
   constructor(props) {
     super(props);
     this.toggleVisibility = this.toggleVisibility.bind(this);
-    var popupStatus = "visible"
+    var popupStatus = "hidden"
     if (typeof localStorage != "undefined") {
       var localPopupStatus = localStorage.getItem('popupStatus')
       console.log(localPopupStatus)
@@ -127,24 +128,25 @@ export default class EmailPopup extends Component {
         <p className="close-popup"
         onClick={() => this.toggleVisibility()}>{this.state.toggleText}
         </p>
-
-          <Form className="form-container" name="Email subscription" method="POST" data-netlify="true" action="/thank-you">
+          <div className="form-container">
+          <Form name="Email subscription" method="POST" data-netlify="true" action={'/thank-you'}>
             <Form.Group controlId="formBasicEmail">
-              {/* <Form.Label>Email address</Form.Label> */}
               <Form.Control
                 name="email"
                 type="email"
                 placeholder="Enter your email"
               />
-              {/* <Form.Text className="text-muted">
-                We will never share your email with anyone else.
-              </Form.Text> */}
             </Form.Group>
 
             <Button className="subscribe-button" variant="primary" type="submit">
               Subscribe to newsletter
             </Button>
             </Form>
+            <Router>
+            <Route path="/thank-you" exact component={Component.ThankYouPage} />
+            </Router>
+            </div>
+            
         {/* </Col>
       </Row> */}
       {/* </div> */}
