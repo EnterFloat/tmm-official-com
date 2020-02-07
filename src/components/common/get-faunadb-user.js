f0d8ca114ce63077a165754e93b5747a74312de2
kasperskov01 <kasperskov01@gmail.com>
2020-02-07 21:28:06 +0100

    Debugging logs

--- src/components/common/get-faunadb-user.js
+++ src/components/common/get-faunadb-user.js
@@ -3,22 +3,26 @@
 import axios from 'axios';
 
 export default function getFaunaDBUser(auth0_id, email) {
-  // console.log('getFaunaDBUser');
+  console.log('getFaunaDBUser');
+  console.log('getFaunaDBUser ' + auth0_id + " " + email);
   return new Promise((resolve, reject) => {
     axios
       .post('/.netlify/functions/netlify-get-faunadb-user', {
         auth0_id: auth0_id,
       })
       .then(res => {
+        console.log('getFaunaDBUser then');
         resolve(res.data);
       })
       .catch(error => {
-        console.log("getFaunaDBUser " + error)
+        console.log("getFaunaDBUser error")
+        console.log("getFaunaDBUser error: " + error)
         console.log(error.response.data.requestResult.statusCode)
         if (error.response.data.requestResult.statusCode === 404 || error.response.data.requestResult.statusCode === 400) {
           console.log("no_faunadb_user")
           resolve('no_faunadb_user');
         }
+        console.log("Other reject")
         reject('getFaunaDBUser error. Netlify function error: ' + error);
       });
   });
